b943fb77e26ca62b5814f04e09ad0acdfd0eb6de
Matthew Lytle <mbl31@pitt.edu>
2022-06-15 13:31:53 -0400

    Added Event class

--- EventsCalendarVariables.py
+++ EventsCalendarVariables.py
@@ -1,10 +1,14 @@
 # File to hold all of the variables being used in the Events Calendar program
 
 # Variables input from user 
-locationRequest = '' # Location to search at for events					: format City,State
-startDateRequest = '000000' # start date to search during for events	: format mmddyy
-endDateRequest = '000000' # end date to search during for events		: format mmddyy
+locationRequest = "" # Location to search at for events					: format City,State
+startDateRequest = "000000" # start date to search during for events	: format mmddyy
+endDateRequest = "000000" # end date to search during for events		: format mmddyy
 
-# Internal variables 
-eventsList = [] # list to hold the name of all events 
-eventsDict = {} # dictionary to hold the event name and date			: format {'name', 'date'}
+# Event class to hold data about a specific event
+class Event:
+	date = "000000" # the date of the event								: format mmddyy
+	day = "" # day of the week the event is  on
+	location = "" # location of the event								: format Venue - City,State
+	price = 0 # price of the event
+	type = "" # type of event, i.e. concert, movie, festival
